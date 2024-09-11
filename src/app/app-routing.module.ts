6f7ded8d2bd934031aac8c2c4aa717acf34ed4f8
Liza <lixia101111@gmail.com>
2024-09-11 17:42:54 +0300

    basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { HighScoreComponent } from './high-score/high-score.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/game', pathMatch: 'full' },
+  { path: 'game', component: GameComponent },
+  { path: 'high-score', component: HighScoreComponent },
+  { path: '**', redirectTo: '/game' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { HighScoreComponent } from './high-score/high-score.component';
+import { GameService } from './game.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    HighScoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GameService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
